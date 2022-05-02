a3e2de6b2868e8d6dc0a81e137468170bf9a7172
Valentin Borisov <akata14@gmail.com>
2022-05-02 12:10:04 +0300

    Fixed bind

--- config.h
+++ config.h
@@ -203,6 +203,7 @@ static MouseShortcut mshortcuts[] = {
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
+#define NEWKEY Mod4Mask
 
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
@@ -210,18 +211,18 @@ static Shortcut shortcuts[] = {
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_K,       zoom,           {.f = +1} },
-	{ TERMMOD,              XK_J,        zoom,           {.f = -1} },
+	{ TERMMOD,              XK_K,       	zoom,           {.f = +1} },
+	{ TERMMOD,              XK_J,        	zoom,           {.f = -1} },
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
 	{ MODKEY,               XK_C,           clipcopy,       {.i =  0} },
 	{ MODKEY,               XK_V,           clippaste,      {.i =  0} },
 	{ MODKEY,               XK_c,           clipcopy,       {.i =  0} },
 	{ MODKEY,               XK_v,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
-	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
+	{ TERMMOD,              XK_y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ MODKEY,               XK_c,           normalMode,     {.i =  0} },
+	{ NEWKEY,               XK_c,           normalMode,     {.i =  0} },
 };
 
 /*
